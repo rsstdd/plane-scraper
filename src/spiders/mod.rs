fde561ac33a3f7cfb53e19b39a062c5f3f169b7b
Ross <rssmtdd@gmail.com>
2023-10-10 13:24:05 -0700

    Run fmt

--- src/spiders/mod.rs
+++ src/spiders/mod.rs
@@ -5,10 +5,10 @@ pub mod plane_phd_manufacturers;
 
 #[async_trait]
 pub trait Spider: Send + Sync {
-    type Item;
+  type Item;
 
-    fn name(&self) -> String;
-    fn start_urls(&self) -> Vec<String>;
-    async fn scrape(&self, url: String) -> Result<(Vec<Self::Item>, Vec<String>), Error>;
-    async fn process(&self, item: Self::Item) -> Result<(), Error>;
+  fn name(&self) -> String;
+  fn start_urls(&self) -> Vec<String>;
+  async fn scrape(&self, url: String) -> Result<(Vec<Self::Item>, Vec<String>), Error>;
+  async fn process(&self, item: Self::Item) -> Result<(), Error>;
 }
--- src/spiders/plane_phd_manufacturers.rs
+++ src/spiders/plane_phd_manufacturers.rs
@@ -7,79 +7,74 @@ use serde::{Deserialize, Serialize};
 use std::time::Duration;
 // use scraper::{Html, Selector};
 use select::{
-    document::Document,
-    predicate::{Attr, Class, Name, Predicate},
+  document::Document,
+  predicate::{Class, Predicate},
 };
 
 pub struct ManufacturersSpider {
-    http_client: Client,
+  http_client: Client,
 }
 
 impl ManufacturersSpider {
-    pub fn new() -> Self {
-        let http_timeout = Duration::from_secs(6);
-        let mut headers = header::HeaderMap::new();
-        headers.insert(
-            "Accept",
-            header::HeaderValue::from_static("application/json"),
-        );
+  pub fn new() -> Self {
+    let http_timeout = Duration::from_secs(6);
+    let mut headers = header::HeaderMap::new();
+    headers.insert("Accept", header::HeaderValue::from_static("application/json"));
 
-        let http_client = Client::builder()
-            .timeout(http_timeout)
-            .default_headers(headers)
-            .user_agent(
-                "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0",
-            )
-            .build()
-            .expect("spiders/github: Building HTTP client");
+    let http_client = Client::builder()
+      .timeout(http_timeout)
+      .default_headers(headers)
+      .user_agent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0")
+      .build()
+      .expect("spiders/github: Building HTTP client");
 
-        ManufacturersSpider { http_client }
-    }
+    ManufacturersSpider { http_client }
+  }
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ManufacturerItem {
-    name: String,
-    link: String,
+  name: String,
+  link: String,
 }
 
 #[async_trait]
 impl super::Spider for ManufacturersSpider {
-    type Item = ManufacturerItem;
+  type Item = ManufacturerItem;
 
-    fn name(&self) -> String {
-        String::from("manufacturer")
+  fn name(&self) -> String {
+    String::from("manufacturer")
+  }
+
+  fn start_urls(&self) -> Vec<String> {
+    vec!["https://planephd.com/wizard/manufacturers/".to_string()]
+  }
+
+  async fn scrape(&self, url: String) -> Result<(Vec<ManufacturerItem>, Vec<String>), Error> {
+    log::info!("visiting: {}", url);
+    let res = self.http_client.get(&url).send().await?;
+    let text = res.text().await?;
+
+    let mut items = Vec::new();
+    let document = Document::from(text.as_str());
+
+    for node in document.find(Class("pp-card").descendant(Class("list-group-item"))) {
+      println!("{:?}", &node.attr("href").unwrap().to_string());
+      let plane = ManufacturerItem {
+        name: node.text(),
+        link: node.attr("href").unwrap().to_string(),
+      };
+      items.push(plane);
     }
 
-    fn start_urls(&self) -> Vec<String> {
-        vec!["https://planephd.com/wizard/manufacturers/".to_string()]
-    }
+    println!("{:?}", items);
 
-    async fn scrape(&self, url: String) -> Result<(Vec<ManufacturerItem>, Vec<String>), Error> {
-        log::info!("visiting: {}", url);
-        let res = self.http_client.get(&url).send().await?;
-        let text = res.text().await?;
+    Ok((items, Vec::new()))
+  }
 
-        let mut items = Vec::new();
-        let document = Document::from(text.as_str());
+  async fn process(&self, item: Self::Item) -> Result<(), Error> {
+    println!("{}", item.name);
 
-        for node in document.find(Class("pp-card").descendant(Class("list-group-item"))) {
-            println!("{:?}", &node.attr("href").unwrap().to_string());
-            let plane = ManufacturerItem {
-                name: node.text(),
-                link: node.attr("href").unwrap().to_string(),
-            };
-            items.push(plane);
-        }
-
-        println!("{:?}", items);
-
-        Ok((items, Vec::new()))
-    }
-
-    async fn process(&self, item: Self::Item) -> Result<(), Error> {
-        println!("{}", item.name);
-
-        Ok(())
-    }
+    Ok(())
+  }
 }
